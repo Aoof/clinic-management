68d9284dd4249c1540a8b3e0478f45b28bed079f
shayan <shayan.delbari@protonmail.com>
2025-11-26 20:58:28 -0500

    Added Doctor and Appointment sevices

---
+++ ClinicManagement_proj/BLL/Services/AppointmentService.cs
@@ -0,0 +1,67 @@
+﻿using ClinicManagement_proj.BLL.DTO;
+using ClinicManagement_proj.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicManagement_proj.BLL.Services
+{
+    internal class AppointmentService
+    {
+        private readonly ClinicDbContext clinicDb = new ClinicDbContext();
+
+        public int CreateAppointment(AppointmentDTO appointmentDTO)
+        {
+            var appointment = new AppointmentDTO
+            {
+                Date = appointmentDTO.Date,
+                Notes = appointmentDTO.Notes,
+                DoctorId = appointmentDTO.DoctorId,
+                PatientId = appointmentDTO.PatientId,
+                TimeSlotId = appointmentDTO.TimeSlotId,
+                CreatedAt = DateTime.Now,
+                ModifiedAt = DateTime.Now,
+            };
+            clinicDb.Appointments.Add(appointment);
+            clinicDb.SaveChanges();
+            return appointment.Id;
+        }
+
+        public bool UpdateAppointment(AppointmentDTO appointmentDTO)
+        {
+            var appointment = clinicDb.Appointments.FirstOrDefault(a => a.Id == appointmentDTO.Id);
+            if (appointment == null) return false;
+
+            appointment.Date = appointmentDTO.Date;
+            appointment.Notes = appointmentDTO.Notes;
+            appointment.DoctorId = appointmentDTO.DoctorId;
+            appointment.PatientId = appointmentDTO.PatientId;
+            appointment.TimeSlotId = appointmentDTO.TimeSlotId;
+            appointment.ModifiedAt = DateTime.Now;
+            clinicDb.SaveChanges();
+            return true;
+        }
+
+        public bool DeleteAppointment(int id)
+        {
+            var appointment = clinicDb.Appointments.Find(id);
+            if (appointment == null) return false;
+
+            clinicDb.Appointments.Remove(appointment);
+            clinicDb.SaveChanges();
+            return true;
+        }
+
+        public AppointmentDTO GetAppointmentById(int id)
+        {
+            return clinicDb.Appointments.FirstOrDefault(a => a.Id == id);
+        }
+
+        public List<AppointmentDTO> GetAllAppointments()
+        {
+            return clinicDb.Appointments.ToList();
+        }
+    }
+}
---
+++ ClinicManagement_proj/BLL/Services/DoctorService.cs
@@ -0,0 +1,63 @@
+﻿using ClinicManagement_proj.BLL.DTO;
+using ClinicManagement_proj.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicManagement_proj.BLL.Services
+{
+    internal class DoctorService
+    {
+        private readonly ClinicDbContext clinicDb = new ClinicDbContext();
+
+        public int CreateDoctor(DoctorDTO doctorDto)
+        {
+            var doctor = new DoctorDTO
+            {
+                FirstName = doctorDto.FirstName,
+                LastName = doctorDto.LastName,
+                LicenseNumber = doctorDto.LicenseNumber,
+                CreatedAt = DateTime.UtcNow,
+                ModifiedAt = DateTime.UtcNow
+            };
+            clinicDb.Doctors.Add(doctor);
+            clinicDb.SaveChanges();
+            return doctor.Id;
+        }
+
+        public bool UpdateDoctor(DoctorDTO doctorDto)
+        {
+            var doctor = clinicDb.Doctors.FirstOrDefault(d => d.Id == doctorDto.Id);
+            if (doctor == null) return false;
+
+            doctor.FirstName = doctorDto.FirstName;
+            doctor.LastName = doctorDto.LastName;
+            doctor.LicenseNumber = doctorDto.LicenseNumber;
+            doctor.ModifiedAt = DateTime.UtcNow;
+            clinicDb.SaveChanges();
+            return true;
+        }
+
+        public bool DeleteDoctor(int id)
+        {
+            var doctor = clinicDb.Doctors.Find(id);
+            if (doctor == null) return false;
+
+            clinicDb.Doctors.Remove(doctor);
+            clinicDb.SaveChanges();
+            return true;
+        }
+
+        public DoctorDTO GetDoctorById(int id)
+        {
+            return clinicDb.Doctors.FirstOrDefault(d => d.Id == id);
+        }
+
+        public List<DoctorDTO> GetAllDoctors()
+        {
+            return clinicDb.Doctors.ToList();
+        }
+    }
+}
